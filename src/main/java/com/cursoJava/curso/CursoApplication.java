e4cd0a2df2db80f5ba1a7a03f82a9efe05e6cef0
Freddy Machaca Mamani <110005596+FreddyMachaca@users.noreply.github.com>
2023-01-04 17:33:15 -0400

    example user system

---
+++ src/main/java/com/cursoJava/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJava.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //indica que es una aplicación Spring Boot
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
